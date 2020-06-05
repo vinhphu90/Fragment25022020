9ad2264041b1d60a5c3d4373669403f0e93f735f
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-06-05 20:33:58 +0700

    pop back stack

--- app/src/main/java/com/examplevinhphutvp/fragment25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/fragment25022020/MainActivity.java
@@ -1,14 +1,73 @@
 package com.examplevinhphutvp.fragment25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+            mFragmentManager = getSupportFragmentManager();
+        }
+
+        public void addAndroid(View view) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            AndroidFragment androidFragment = new AndroidFragment();
+            fragmentTransaction.add(R.id.liearlayoutContainer,androidFragment,"fragmentandroid");
+            fragmentTransaction.addToBackStack("FragmentAndroid");
+            fragmentTransaction.commit();
+        }
+
+        public void addIos(View view) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            IosFragment iosFragment = new IosFragment();
+            fragmentTransaction.add(R.id.liearlayoutContainer,iosFragment,"fragmentios");
+            fragmentTransaction.addToBackStack("FragmentIos");
+            fragmentTransaction.commit();
+        }
+
+        public void replaceAndroid(View view) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            AndroidFragment androidFragment = new AndroidFragment();
+            fragmentTransaction.replace(R.id.liearlayoutContainer,androidFragment);
+            fragmentTransaction.commit();
+        }
+
+        public void replaceIos(View view) {
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            IosFragment iosFragment = new IosFragment();
+            fragmentTransaction.replace(R.id.liearlayoutContainer,iosFragment);
+            fragmentTransaction.commit();
+        }
+//    public void removeAndroid(View view) {
+//        AndroidFragment androidFragment =
+//                (AndroidFragment) mFragmentManager.findFragmentByTag("fragmentandroid");
+//        if (androidFragment != null){
+//            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+//            fragmentTransaction.remove(androidFragment);
+//            fragmentTransaction.commit();
+//        }
+//    }
+//
+//    public void removeIos(View view) {
+//        IosFragment iosFragment = (IosFragment) mFragmentManager.findFragmentByTag("fragmentios");
+//        if (iosFragment != null){
+//            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+//            fragmentTransaction.remove(iosFragment);
+//            fragmentTransaction.commit();
+//        }
+        public void popbackstack(View view) {
+            mFragmentManager.popBackStack( 2, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+
     }
-}
+
